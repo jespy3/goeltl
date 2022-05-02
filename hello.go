8ce0e18d3f127fabbe31ed59beb0585a16d14f02
josh-espinas <josh.espinas@xero.com>
2022-05-02 22:28:29 +1200

    Writes first API call against a pokemon api

--- hello.go
+++ hello.go
@@ -1,13 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
   fmt.Println("Welcome to Goeltl, the Poeltl helper.")
   fmt.Println("Make your first guess of an NBA player...")
+
+  fmt.Println("Jokes. Let's start with Pokemon for now...")
+  uri := "http://pokeapi.co/api/v2/pokedex/kanto/"
+  pokemon := getAllPokemon(uri)
+  fmt.Println(pokemon)
 }
 
 func WritesHelloWorld() string {
   result := "Hello World!"
   return result
+}
+
+type pokemon struct {
+  entry_number    int
+  pokemon_species species
+}
+
+type species struct {
+  name string
+  url  string
+}
+
+func getAllPokemon(uri string) *http.Response {
+  res, err := http.Get(uri)
+
+  if err != nil {
+    fmt.Print(err.Error())
+  }
+
+  // fmt.Printf("response = %v", res)
+  fmt.Printf("res.Status = %v\n", res.Status)
+  fmt.Printf("res.StatusCode = %v\n", res.StatusCode)
+
+  data, err := ioutil.ReadAll(res.Body)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println(string(data))
+
+  return res
 }
--- hello_test.go
+++ hello_test.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-  "testing"
+	"fmt"
+	"testing"
 )
+
+func FailedTest(actual, expected string) string {
+  return fmt.Sprintf("Got: %v, Expected: %v", actual, expected)
+}
 
 func TestWritesHelloWorld(t *testing.T) {
   ans := WritesHelloWorld()
@@ -12,3 +17,36 @@ func TestWritesHelloWorld(t *testing.T) {
       "want WritesHelloWorld() = \"Hello World!\"", ans)
   }
 }
+
+func TestBaseUrl(t *testing.T) {
+  t.Run("returns 200", func(t *testing.T) {
+    // given
+    uri := "http://pokeapi.co/api/v2/pokedex/kanto/"
+
+    // when
+    res := getAllPokemon(uri)
+
+    // then
+    actual := fmt.Sprintf("%v", res.StatusCode)
+    expected := "200"
+
+    if actual != expected {
+      t.Errorf(FailedTest(actual, expected))
+    }
+  })
+}
+
+// func TestSpeaker(t *testing.T) {
+//   t.Run("calls Speaker", func(t *testing.T) {
+//     f := &fakeSpeaker{}
+
+//     Hello(f, "GoCon")
+
+//     actual := f.CallCount
+//     expected := 1
+
+//     if actual != expected {
+//       t.Errorf("Got: %v, Expected: %v", actual, expected)
+//     }
+//   })
+// }
